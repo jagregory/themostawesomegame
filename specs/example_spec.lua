935c1e2408070b82652a68c227cf20ad8e027b8a
James Gregory <james@jagregory.com>
2014-03-06 15:34:51 +1100

    Specs

---
+++ specs/example_spec.lua
@@ -0,0 +1,7 @@
+describe("Busted unit testing framework", function()
+  describe("should be awesome", function()
+    it("should be easy to use", function()
+      assert.truthy("Yup.")
+    end)
+  end)
+end)
